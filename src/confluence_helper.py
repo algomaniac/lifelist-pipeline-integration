d3a4671e8129e8a8da6e696973936b94183879f2
Tapan Chandra <tapan.nallan@jda.com>
2017-01-29 21:10:30 +0530

    updating confluence page

--- src/confluence_helper.py
+++ src/confluence_helper.py
@@ -1,15 +1,20 @@
 import base64
 import os
 import pyconfluence
-from pyconfluence.actions import get_page_content
+from pyconfluence.actions import edit_page,get_page_content
+
+
+def set_confluence_environment():
+    os.environ['PYCONFLUENCE_TOKEN'] = base64.b64decode( os.environ['AUTH'].encode( 'utf-8' ) ).decode( 'utf-8' ).split(':')[1].strip()
+    os.environ['PYCONFLUENCE_USER'] = 'j1013225'
+    os.environ['PYCONFLUENCE_URL'] = 'https://confluence.jda.com'
 
 
 def update_lifelist_page(xhtml):
-    pass
-    
-os.environ['PYCONFLUENCE_TOKEN'] = base64.b64decode( os.environ['AUTH'].encode( 'utf-8' ) ).decode( 'utf-8' ).split(':')[1].strip()
-os.environ['PYCONFLUENCE_USER'] = 'j1013225'
-os.environ['PYCONFLUENCE_URL'] = 'https://confluence.jda.com'
+    set_confluence_environment()
+    edit_page('154903335', 'What am I doing with my life', '~j1013225', xhtml)
 
-
-print(get_page_content('154903335'))
+def get_lifelist_page_contents():
+    set_confluence_environment()
+    print(get_page_content('154903335'))
+       
--- src/main.py
+++ src/main.py
@@ -1,22 +1,43 @@
+# -*- coding: utf-8 -*-
+
 import os
 
-from trello.exceptions import *
-
 import trellohelper
-from confluence_helper import update_lifelist_page
+import constants
+
+from trello.exceptions import *
+from confluence_helper import update_lifelist_page,get_lifelist_page_contents
+
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 
-def generate_lifelist_xhtml(trello_data):
-    pass
+env = Environment(
+    loader = PackageLoader('main', 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+def generate_lifelist_xhtml(trello_data):  
+    template = env.get_template('page.xhtml')
+
+    return template.render(data=trello_data,domains=constants.project_domains) #,domain_prefixes=constants.reverse_project_domain_map
 
 
 def main():
     try:
+        
+        get_lifelist_page_contents()
+        print('done')
+        exit()
+
         data = trellohelper.get_pipeline_data()   
 
         xhtml = generate_lifelist_xhtml(data)
 
+        #print(xhtml)
+
         update_lifelist_page(xhtml)
+
+        print('Done')
 
 
     except Unauthorized :
