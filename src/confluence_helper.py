87281b3f67dec2df8f23c569aa6d4173b8fd1e8d
Tapan Chandra <tapan.nallan@jda.com>
2017-01-30 12:22:15 +0530

    adding stats by domains

--- src/confluence_helper.py
+++ src/confluence_helper.py
@@ -3,6 +3,12 @@ import os
 import pyconfluence
 from pyconfluence.actions import edit_page,get_page_content
 
+htmlCodes = [
+    ['&', '&amp;'],
+    ['<', '&lt;'],
+    ['>', '&gt;'],
+    ['"', '&quot;'],
+]
 
 def set_confluence_environment():
     os.environ['PYCONFLUENCE_TOKEN'] = base64.b64decode( os.environ['AUTH'].encode( 'utf-8' ) ).decode( 'utf-8' ).split(':')[1].strip()
@@ -17,4 +23,10 @@ def update_lifelist_page(xhtml):
 def get_lifelist_page_contents():
     set_confluence_environment()
     print(get_page_content('154903335'))
-       
+
+def htmlEncode(s, codes=htmlCodes):
+    """ Returns the HTML encoded version of the given string. This is useful to
+        display a plain ASCII text string on a web page."""
+    for code in codes:
+        s = s.replace(code[0], code[1])
+    return s
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import constants
 import dateutils
 
 from trello.exceptions import *
-from confluence_helper import update_lifelist_page,get_lifelist_page_contents
+from confluence_helper import update_lifelist_page,get_lifelist_page_contents,htmlEncode
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -16,6 +16,20 @@ env = Environment(
     loader = PackageLoader('main', 'templates'),
     autoescape=select_autoescape(['html', 'xml'])
 )
+
+def generate_completed_entry_count_by_domain(completed_entries_map):
+    
+    entries_count = {'None':0}
+
+    for domain in constants.project_domains:
+        entries_count[domain] = 0
+
+    for key,entries in completed_entries_map.items():
+
+        for entry in entries:
+            entries_count[entry["domain"]] += 1
+
+    return entries_count
 
 def generate_template_data(trello_data):
     template_data = {}
@@ -47,6 +61,23 @@ def generate_template_data(trello_data):
         template_data["completed_task_queue"].insert(0,result)
 
         start_month_year = dateutils.increment_month(*start_month_year)
+    
+    #Generate Stats by domain
+    entries_count_by_domain = generate_completed_entry_count_by_domain(trello_data["completed_tasks_queue"])
+
+    template_data["task_count_by_domain"] = {}
+
+    for domain in sorted(constants.project_domains):
+        domain_text = htmlEncode(constants.project_domains[domain])
+
+        count_in_backlog = len( [entry for entry in trello_data["infinite_task_queue"] if entry["domain"] == domain] )
+        count_in_bucket = len( [entry for entry in trello_data["bucket_task_queue"] if entry["domain"] == domain] )
+        count_in_progress = len( [entry for entry in trello_data["in_progress_task_queue"] if entry["domain"] == domain] )
+        count_in_suspended = len( [entry for entry in trello_data["suspended_task_queue"] if entry["domain"] == domain] )
+        count_in_completed = entries_count_by_domain[domain]
+        count_total = count_in_backlog + count_in_bucket + count_in_progress + count_in_suspended + count_in_completed
+
+        template_data["task_count_by_domain"][domain] = {'domain_prefix':domain,'domain_text':domain_text,'backlog':str(count_in_backlog),'bucket':str(count_in_bucket),'suspended':str(count_in_suspended),'inprogress':str(count_in_progress),'completed':str(count_in_completed),'total':str(count_total)}
 
     return template_data
 
